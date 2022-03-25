ffcf6f0005be02cc4d6b9f60c4b6a7a990f37d15
Eduardo Araujo <eduardojafaraujo@gmail.com>
2022-03-25 14:39:14 +0000

    topo1

---
+++ tp1Ex1Topo.py
@@ -0,0 +1,22 @@
+from mininet.topo import Topo
+
+class Ex1Topo( Topo ):
+    "Ex1 topology."
+
+    def build( self ):
+        "Create custom topo."
+
+        # Add hosts and switches
+        switch = self.addSwitch( 's1' )
+        firstHost = self.addHost( 'h1' )
+        secondHost = self.addHost( 'h2' )
+        thirdHost = self.addHost( 'h3' )
+        fourthHost = self.addHost( 'h4' )
+        
+        # Add links
+        self.addLink( switch, firstHost )
+        self.addLink( switch, secondHost )
+        self.addLink( switch, thirdHost )
+        self.addLink( switch, fourthHost )
+
+topos = { 'ex1topo': ( lambda: Ex1Topo() ) }
