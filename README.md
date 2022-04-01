60b636523dd003bc7576050afb31dc65fdfdb023
Eduardo Araujo <eduardojafaraujo@gmail.com>
2022-04-01 14:57:40 +0100

    topo2done

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # SDN2122
+sudo mn --custom tp1Ex2Topo.py --topo ex2topo --controller remote
--- tp1Ex2Topo.py
+++ tp1Ex2Topo.py
@@ -1,31 +1,30 @@
 from mininet.topo import Topo
 
-class Ex1Topo(Topo):
+class Ex2Topo(Topo):
     "Ex2 topology."
 
     def build(self):
-
-        # Add hosts and switches
+        #Add hosts and switches
         switchL3 = self.addSwitch('sL3')
 
 
-        switchL2_1 = self.addSwitch('s1')
-        host1 = self.addHost('h1')
-        host2 = self.addHost('h2')
-        host3 = self.addHost('h3')
+        switchL2_1 = self.addSwitch('sL2_1')
+        host1 = self.addHost('h1',ip="10.0.1.1/24")
+        host2 = self.addHost('h2',ip="10.0.1.2/24")
+        host3 = self.addHost('h3',ip="10.0.1.3/24")
         
-        switchL2_2 = self.addSwitch('s2')
-        host4 = self.addHost('h4')
-        host5 = self.addHost('h5')
-        host6 = self.addHost('h6')
+        switchL2_2 = self.addSwitch('sL2_2')
+        host4 = self.addHost('h4',ip="10.0.2.1/24")
+        host5 = self.addHost('h5',ip="10.0.2.2/24")
+        host6 = self.addHost('h6',ip="10.0.2.3/24")
         
-        switchL2_3 = self.addSwitch('s3')
-        host7 = self.addHost('h7')
-        host8 = self.addHost('h8')
-        host9 = self.addHost('h9')
+        switchL2_3 = self.addSwitch('sL2_3')
+        host7 = self.addHost('h7',ip="10.0.3.1/24")
+        host8 = self.addHost('h8',ip="10.0.3.2/24")
+        host9 = self.addHost('h9',ip="10.0.3.3/24")
         
-        # Add links
-        self.addLink(switchL3, switchL2_1)
+        #Add links
+        self.addLink(switchL3, switchL2_1, delay='5ms')
         self.addLink(switchL3, switchL2_2)
         self.addLink(switchL3, switchL2_3)
         
@@ -35,11 +34,12 @@ class Ex1Topo(Topo):
         
         self.addLink(switchL2_2, host4)
         self.addLink(switchL2_2, host5)
-        self.addLink(switchL2_2, host6)
+        self.addLink(switchL2_2, host6, delay='5ms')
         
         self.addLink(switchL2_3, host7)
         self.addLink(switchL2_3, host8)
-        self.addLink(switchL2_3, host9)
+        self.addLink(switchL2_3, host9, loss=10)
+
+      
         
-        
-topos = {'ex1topo': (lambda: Ex1Topo())}
+topos = {'ex2topo': (lambda: Ex2Topo())}
