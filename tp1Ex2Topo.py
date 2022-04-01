f53afc7ee6504f689795e419ce20a0eba87583d4
Eduardo Araujo <eduardojafaraujo@gmail.com>
2022-04-01 14:30:41 +0100

    ex2topo

--- tp1Ex2Topo.py
+++ tp1Ex2Topo.py
@@ -1,21 +1,45 @@
 from mininet.topo import Topo
 
-class Ex1Topo( Topo ):
+class Ex1Topo(Topo):
     "Ex2 topology."
 
-    def build( self ):
+    def build(self):
 
         # Add hosts and switches
-        switch = self.addSwitch( 's1' )
-        firstHost = self.addHost( 'h1' )
-        secondHost = self.addHost( 'h2' )
-        thirdHost = self.addHost( 'h3' )
-        fourthHost = self.addHost( 'h4' )
+        switchL3 = self.addSwitch('sL3')
+
+
+        switchL2_1 = self.addSwitch('s1')
+        host1 = self.addHost('h1')
+        host2 = self.addHost('h2')
+        host3 = self.addHost('h3')
+        
+        switchL2_2 = self.addSwitch('s2')
+        host4 = self.addHost('h4')
+        host5 = self.addHost('h5')
+        host6 = self.addHost('h6')
+        
+        switchL2_3 = self.addSwitch('s3')
+        host7 = self.addHost('h7')
+        host8 = self.addHost('h8')
+        host9 = self.addHost('h9')
         
         # Add links
-        self.addLink( switch, firstHost )
-        self.addLink( switch, secondHost )
-        self.addLink( switch, thirdHost )
-        self.addLink( switch, fourthHost )
-
-topos = { 'ex1topo': ( lambda: Ex1Topo() ) }
+        self.addLink(switchL3, switchL2_1)
+        self.addLink(switchL3, switchL2_2)
+        self.addLink(switchL3, switchL2_3)
+        
+        self.addLink(switchL2_1, host1)
+        self.addLink(switchL2_1, host2)
+        self.addLink(switchL2_1, host3)
+        
+        self.addLink(switchL2_2, host4)
+        self.addLink(switchL2_2, host5)
+        self.addLink(switchL2_2, host6)
+        
+        self.addLink(switchL2_3, host7)
+        self.addLink(switchL2_3, host8)
+        self.addLink(switchL2_3, host9)
+        
+        
+topos = {'ex1topo': (lambda: Ex1Topo())}
